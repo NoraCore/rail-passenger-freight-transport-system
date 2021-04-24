5cef5bfbb201affcd5fed5913b6ac484bcde9260
Julia <14entropiia88@gmail.com>
2021-04-24 21:50:30 +0300

    Add runner in view module.

---
+++ train-view-system/src/main/java/by/issoft/krivonos/train/Runner.java
@@ -0,0 +1,10 @@
+package by.issoft.krivonos.train;
+
+import by.issoft.krivonos.train.domens.wagons.ExampleTrainLinks;
+
+public class Runner {
+    public static void main(String[] args) {
+        ExampleTrainLinks train = new ExampleTrainLinks();
+
+    }
+}
